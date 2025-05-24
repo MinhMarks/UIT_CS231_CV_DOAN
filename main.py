b9e5964bc36215af79657e14d828c25b9d35e285
MinhMark <minhnhat21012005@gmail.com>
2025-05-24 03:36:18 +0000

    adfasd

--- main.py
+++ main.py
@@ -18,12 +18,17 @@ if __name__ == '__main__':
     
     model = VPRModel(
         #---- Encoder
-        backbone_arch='dinov2_vitb14',
+        # backbone_arch='dinov2_vitb14',
+        # backbone_config={
+        #     'num_trainable_blocks': 4,
+        #     'return_token': True,
+        #     'norm_layer': True,
+        # },
+
+        backbone_arch='resnet16',
         backbone_config={
-            'num_trainable_blocks': 4,
-            'return_token': True,
-            'norm_layer': True,
         },
+        
         agg_arch='SALAD',
         agg_config={
             'num_channels': 768,
